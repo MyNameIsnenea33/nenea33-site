74579f8be6892c12d428d50e0aba89c9f5a40a4f
nenea33 <81491357+MyNameIsnenea33@users.noreply.github.com>
2022-04-11 20:00:50 +0300

    Add files via upload

---
+++ phpBB2/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// phpBB 2.x auto-generated config file
+// Do not change anything in this file!
+
+$dbms = 'mysql';
+
+$dbhost = 'sql102.epizy.com';
+$dbname = 'epiz_31470253_XXX';
+$dbuser = 'epiz_31470253';
+$dbpasswd = '90b3c3JiD0fiOJm';
+
+$table_prefix = 'phpbb_';
+
+define('PHPBB_INSTALLED', true);
+
+?>
